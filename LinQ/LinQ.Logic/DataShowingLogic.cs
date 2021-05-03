2bd8e2276ac189eb27896194c5fc6f5f8dbddcc7
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-03 12:02:42 -0300

    Add files via upload

---
+++ LinQ/LinQ.Logic/DataShowingLogic.cs
@@ -0,0 +1,36 @@
+﻿using LinQ.Data;
+using LinQ.Entities;
+using LinQ.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ.Logic
+{
+    public class DataShowingLogic
+    {
+        public void showCustomers(List<Customers> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.CustomerID} {item.ContactName}");
+            }
+        }
+        public void showProducts(List<Products> query)
+        {
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.ProductID} {item.ProductName}");
+            }
+        }
+        public void showNames(IQueryable<string> query)
+        {
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
