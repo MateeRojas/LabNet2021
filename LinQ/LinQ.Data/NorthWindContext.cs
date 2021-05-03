6e4c3e8f044ab8bccfd812d0dae6c688d9a50314
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-03 12:01:04 -0300

    Add files via upload

---
+++ LinQ/LinQ.Data/NorthWindContext.cs
@@ -0,0 +1,45 @@
+using LinQ.Entities;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LinQ.Data
+{
+    public partial class NorthWindContext : DbContext
+    {
+        public NorthWindContext()
+            : base("name=NorthWindConection")
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Customers> Customers { get; set; }
+        public virtual DbSet<Orders> Orders { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<Territories> Territories { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customers>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Orders>()
+                .Property(e => e.CustomerID)
+                .IsFixedLength();
+
+            modelBuilder.Entity<Orders>()
+                .Property(e => e.Freight)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Products>()
+                .Property(e => e.UnitPrice)
+                .HasPrecision(19, 4);
+
+            modelBuilder.Entity<Territories>()
+                .Property(e => e.TerritoryDescription)
+                .IsFixedLength();
+        }
+    }
+}
