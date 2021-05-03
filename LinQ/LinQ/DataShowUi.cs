0b8e412bad469c401285f6a889619b5d8a36daa2
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-03 19:44:02 -0300

    Add files via upload

---
+++ LinQ/LinQ/DataShowUi.cs
@@ -0,0 +1,34 @@
+﻿using LinQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ
+{
+    class DataShowUi
+    {
+        public void showCustomers(List<Customers> list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine($"{item.CustomerID} {item.ContactName}");
+            }
+        }
+        public void showProducts(List<Products> query)
+        {
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.ProductID} {item.ProductName}");
+            }
+        }
+        public void showNames(IQueryable<string> query)
+        {
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
