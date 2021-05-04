917921b02f34030e5965023ee0518a2b34237f45
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-04 17:37:07 -0300

    Add files via upload

---
+++ ExtensionMethods + Exceptions + Unit Test/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("999c0275-be69-4b2f-833f-260b07f6a626")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExtensionMethods + Exceptions + Unit Test/Tests/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using ExtensionMethods___Exceptions___Unit_Test.Extensions;
+using ExtensionMethods___Exceptions___Unit_Test.Excepcions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test01DividoPorCero()
+        {
+            int numero = 10;
+            bool huboExcepcion = numero.DividirPorCero();
+            Assert.AreEqual(huboExcepcion, true);
+        }
+
+        [TestMethod]
+        public void Test02DividoConArgumentosValidos()
+        {
+            int numero = 10;
+            double resultado = numero.Dividir(2);
+            Assert.AreEqual(5, resultado);
+        }
+        [TestMethod]
+        public void Test03DividoConArgumentosNoValidos()
+        {
+            int numero = 10;
+            double resultado = numero.Dividir(null);
+            Assert.AreEqual(0, resultado);
+        }
+        [TestMethod]
+        public void Test04TiroUnaExcepcionCualquiera()
+        {
+            Logic logic = new Logic();
+            Assert.ThrowsException<IndexOutOfRangeException>(() => logic.TirarExcepcion());
+        }
+
+        [TestMethod]
+        public void Test05TiroUnaExcepcionPersonalizada()
+        {
+            Logic logic = new Logic();
+            Assert.ThrowsException<ExcepcionPersonalizada>(() => logic.TirarExcepcionPersonalizada());
+        }
+    }
+}
