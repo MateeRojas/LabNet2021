ac4ebf86775143ef197ab771fb7116e08ead38aa
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-10 16:19:40 -0300

    Delete LabSQLEFWeb directory

--- LabSQLEFWeb/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace LabSQLEFWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- LabSQLEFWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace LabSQLEFWeb.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
