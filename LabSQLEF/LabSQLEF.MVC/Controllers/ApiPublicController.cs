f4576eeff74535dd1fde86da6f33546d6fd5d7fd
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-10 16:01:21 -0300

    Tp 9 WebApi

---
+++ LabSQLEF/LabSQLEF.MVC/Controllers/ApiPublicController.cs
@@ -0,0 +1,32 @@
+﻿using LabSQLEF.Logic;
+using LabSQLEF.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabSQLEF.MVC.Controllers
+{
+    public class ApiPublicController : Controller
+    {
+        // GET: ApiPublic
+        ApiPublicaLogic logic = new ApiPublicaLogic();
+
+        public ActionResult Index()
+        {
+            try
+            {
+
+                ApiView view = new ApiView();
+                view.content = logic.GetItem();
+
+                return View(view);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+    }
+}
