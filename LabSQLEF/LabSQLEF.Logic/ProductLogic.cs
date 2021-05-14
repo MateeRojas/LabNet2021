21598d5a611adde25c9a87726985804582a9a89a
MateeRojas <62266750+MateeRojas@users.noreply.github.com>
2021-05-14 17:36:41 -0300

    Add files via upload

---
+++ LabSQLEF/LabSQLEF.Logic/ProductLogic.cs
@@ -0,0 +1,43 @@
+﻿using LabSQLEF.Data;
+using LabSQLEF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSQLEF.Logic
+{
+    public class ProductLogic : BaseLogic, ILogic<Products, int>
+    {
+        public void Add(Products newItem)
+        {
+            context.Products.Add(newItem);
+            context.SaveChanges();
+        }
+
+        public List<Products> GetAll()
+        {
+            return context.Products.ToList();
+        }
+
+        public void Update(Products prod)
+        {
+            var toUpdate = context.Products.Find(prod.ProductID);
+                toUpdate.UnitPrice = prod.UnitPrice;
+                context.SaveChanges();
+        }      
+
+        public Products Find(int id)
+        {
+            return context.Products.Find(id);
+        }
+
+        public void Delete(int id)
+        {
+            var toDelete = context.Products.Find(id);
+            context.Products.Remove(toDelete);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ LabSQLEF/LabSQLEF.Logic/TerritoryLogic.cs
@@ -0,0 +1,45 @@
+﻿using LabSQLEF.Data;
+using LabSQLEF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabSQLEF.Logic
+{
+    public class TerritoryLogic : BaseLogic, ILogic<Territories , string>
+    {
+
+        public List<Territories> GetAll()
+        {
+            return context.Territories.ToList();
+        }
+
+        public void Add(Territories newTerritory)
+        {
+            context.Territories.Add(newTerritory);
+            context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            var toDelete = context.Territories.Find(id);
+            context.Territories.Remove(toDelete);
+            context.SaveChanges();
+        }
+
+        public Territories Find(string id)
+        {
+            return context.Territories.Find(id);
+        }
+
+        public void Update(Territories ter)
+        {
+            var toUpdate = context.Territories.Find(ter.TerritoryID);
+            toUpdate.TerritoryDescription = ter.TerritoryDescription;
+
+            context.SaveChanges();
+        }
+    }
+}
